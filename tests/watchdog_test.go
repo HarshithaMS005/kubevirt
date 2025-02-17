1b9856b1157719a01aa728c4af05efae6736faa5
Harshitha MS <harshitha.ms@ibm.com>
2025-02-17 13:55:41 +0530

    Watch_dog_test_changes

--- tests/watchdog_test.go
+++ tests/watchdog_test.go
@@ -20,6 +20,7 @@
 package tests_test
 
 import (
+	"runtime"
 	"time"
 
 	"kubevirt.io/kubevirt/tests/decorators"
@@ -43,11 +44,58 @@ var _ = Describe("[sig-compute]Watchdog", decorators.SigCompute, func() {
 	Context("A VirtualMachineInstance with a watchdog device", func() {
 
 		It("[test_id:4641]should be shut down when the watchdog expires", decorators.Conformance, func() {
+			arch := runtime.GOARCH
+
 			vmi := libvmops.RunVMIAndExpectLaunch(
-				libvmifact.NewAlpine(libvmi.WithWatchdog(v1.WatchdogActionPoweroff)), 360)
+				libvmifact.NewFedora(libvmi.WithWatchdog(v1.WatchdogActionPoweroff)), 360)
 
 			By("Expecting the VirtualMachineInstance console")
-			Expect(console.LoginToAlpine(vmi)).To(Succeed())
+			Expect(console.LoginToFedora(vmi)).To(Succeed())
+
+			if arch == "s390x" {
+				By("Loading the diag288_wdt watchdog module")
+				Expect(console.SafeExpectBatch(vmi, []expect.Batcher{
+					&expect.BSnd{S: "sudo modprobe diag288_wdt\n"},
+					&expect.BExp{R: console.PromptExpression},
+				}, 250)).To(Succeed())
+
+				By("Verifying if the module is loaded")
+				Expect(console.SafeExpectBatch(vmi, []expect.Batcher{
+					&expect.BSnd{S: "lsmod | grep diag288_wdt\n"},
+					&expect.BExp{R: "diag288_wdt"},
+				}, 250)).To(Succeed())
+
+			}
+
+			By("Installing watchdog package")
+			Expect(console.SafeExpectBatch(vmi, []expect.Batcher{
+				&expect.BSnd{S: "sudo dnf install -y watchdog\n"},
+				&expect.BExp{R: console.PromptExpression},
+			}, 250)).To(Succeed())
+
+			By("Uncommenting watchdog-device line in /etc/watchdog.conf")
+			Expect(console.SafeExpectBatch(vmi, []expect.Batcher{
+				&expect.BSnd{S: "sudo sed -i 's/^#watchdog-device/watchdog-device/' /etc/watchdog.conf\n"},
+				&expect.BExp{R: console.PromptExpression},
+			}, 250)).To(Succeed())
+
+			By("Starting watchdog service")
+			Expect(console.SafeExpectBatch(vmi, []expect.Batcher{
+				&expect.BSnd{S: "sudo systemctl start watchdog\n"},
+				&expect.BExp{R: console.PromptExpression},
+			}, 250)).To(Succeed())
+
+			By("Verifying watchdog service is running")
+			Expect(console.SafeExpectBatch(vmi, []expect.Batcher{
+				&expect.BSnd{S: "systemctl status watchdog | grep 'Active:'\n"},
+				&expect.BExp{R: "Active: active"},
+			}, 250)).To(Succeed())
+
+			By("Checking watchdog device presence")
+			Expect(console.SafeExpectBatch(vmi, []expect.Batcher{
+				&expect.BSnd{S: "ls /dev/watchdog\n"},
+				&expect.BExp{R: "/dev/watchdog"},
+			}, 250)).To(Succeed())
 
 			By("Killing the watchdog device")
 			Expect(console.SafeExpectBatch(vmi, []expect.Batcher{
